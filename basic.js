d3d55dc27016904f1b455444fa07ccc2cc8ba5d9
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-08-23 20:20:17 +0545

    Basics of javascript

---
+++ basic.js
@@ -0,0 +1,9 @@
+const person={
+    name:'Deepika',
+    age:12,
+    city:"Newyork"
+};
+//printing each property using dot notation and bracket notation
+console.log(person.name);
+console.log(person['age']);
+console.log(person.city);
