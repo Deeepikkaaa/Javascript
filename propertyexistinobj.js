c4911f792d80fe2d12c1f0bb30c0659eff748ca6
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-09-09 11:30:59 +0545

    Checking property exists or not in object using in operator

---
+++ propertyexistinobj.js
@@ -0,0 +1,15 @@
+//You can check if a property exists in an object using the in operator:
+const obj = {
+    name: 'Prakash',
+    age: 99,
+    city: 'Humla'
+};
+
+const isPropertyFound = 'name' in obj;
+console.log(isPropertyFound); // Output: true
+
+const isAgePropertyFound = 'age' in obj;
+console.log(isAgePropertyFound); // Output: true
+
+const isCountryPropertyFound = 'country' in obj;
+console.log(isCountryPropertyFound); // Output: false
