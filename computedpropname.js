eeaefae647a8a68c83585fc86e98402427b3b0d7
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-09-11 15:18:11 +0545

    two methods of declaring the object with computed property name

--- computedpropname.js
+++ computedpropname.js
@@ -6,3 +6,10 @@ let fullname={
     [LAST_NAME]:"Bhandari"
 };
 console.log("My full name is: "+fullname.firstname+" "+fullname.lastname);
+//Method 2 you first create the object normally, then assign new properties using square brackets.
+let LAST_NAME1="lastname1";
+let fullname1={
+    firstname1:"Deepika",
+    };
+    fullname1[LAST_NAME1]="Bhandari";
+    console.log("My full name is: "+fullname1.firstname1+" "+fullname1.lastname1);
