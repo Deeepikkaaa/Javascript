9f7060cb83b8603d6a83d0d618883beba3c9c586
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-09-11 19:11:10 +0545

    Shorthhand syntax for object property value in ES6

---
+++ shorthhand.js
@@ -0,0 +1,14 @@
+//Shorthhand syntax for object property value in ES6
+//in js variable name and object property name are same we don't need to repeat it
+const name="Rajkumari";
+const age=18;
+const address="Nepal";
+const Nepal="A county in South Asia where the world's highest peak Mt Everest is located,where the light of Asia Gautam Buddha was born!!";
+  
+const user={
+    name,
+    age,
+    address,
+    Nepal
+};
+console.log(user);
