c0dbe3988e460a7ce6be41c167fcf95aa9c0e006
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-09-17 14:49:21 +0545

    JS string to lowercase

---
+++ strtolowecase.js
@@ -0,0 +1,10 @@
+// js string toLowerCase() Method
+//converts string to lowercase
+let flower="LALIGURASH";
+let lowercase=flower.toLowerCase();
+console.log(lowercase);
+
+//converting elements of array to lowercase
+ let temples=["PASHUPATINATH","SWOYAMBUNATH","CHANGUNARAYAN"];
+ let result=temples.map(temp=>temp.toLowerCase());
+ console.log(result);
