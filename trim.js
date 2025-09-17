1baf577c63833f9daf1e5023db7048d4ed4ad2f2
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-09-17 20:30:29 +0545

    js string trim() method

--- trim.js
+++ trim.js
@@ -1,5 +1,5 @@
 //js string trim() method
 // used to trim unnecessary spaces from string or user input data
-let s="   DeepikaBhandari";
+let s="   DeepikaBhandari     ";
 let s1=s.trim();
 console.log(s1);
