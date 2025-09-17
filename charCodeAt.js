8b5f3eeb28e47760d9416e22237747687721103c
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-09-17 12:56:00 +0545

    JS string charCodeAt()

---
+++ charCodeAt.js
@@ -0,0 +1,7 @@
+//js String charCodeAt()method returns the unicode of character whose index is provided
+function Deepest(){
+    let river="Congo";
+    let value=river.charCodeAt(4);
+    console.log(value);
+} 
+Deepest();
