18510497eaaaa8939b1fb4d6e693b5cbb10632f7
Deeepikkaaa <deepikabhandari294@gmail.com>
2025-10-20 16:32:04 +0545

    Form validation practice

--- DOM/locationsearch.js
+++ DOM/locationsearch.js
@@ -1,12 +1,9 @@
 let button=document.querySelector(".button");
 
 button.addEventListener(("click"),(e)=>{
-   
-
 let url=document.createElement("a");
 url.setAttribute("href","https://github.com/Deeepikkaaa?tab=repositories");
  if(e.target.classList.contains("button")){    
-
 alert("URL search value is: " + url.search);
     };
    
